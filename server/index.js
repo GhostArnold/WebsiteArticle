6196a31bb96d0f7518e1070cf0a5609ac1a38f54
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-07-30 13:55:48 +0300

    Base setting express server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+// Импорт библиотеки для взаимодейсвтия с базой данных
+import mongoose from 'mongoose';
+// Импорт пакета для env переменных
+import dotenv from 'dotenv';
+// Для устранения конфликтов между разными доменами
+import cors from 'cors';
+
+const app = express();
+// Загрузка env переменных из .env файла
+dotenv.config();
+
+// Contstants
+const PORT = parseInt(process.env.PORT) || 3001; // ППорт из .env или 3001
+
+// Middleware
+// Можем отправлять запросы к бэкэнду с разных айпи адресов и доменов
+app.use(cors());
+//  Эта строка подключает middleware, который позволяет вашему приложению обрабатывать JSON-формат запросов.
+app.use(express.json());
+
+// Так как все операции с базой данных являются ассинхронными
+async function start() {
+  try {
+    // Подключаемся к базе данных
+    await mongoose.connect('mongodb://localhost:27017/articleWebsite');
+    // Запускаем сервер
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+start();
