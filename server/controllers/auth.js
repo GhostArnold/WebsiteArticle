aae58f999b2f2f48d6bee25707a2d42be7805113
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-08-04 16:22:38 +0300

    I made an autorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -51,7 +51,54 @@ export const register = async (req, res) => {
 };
 
 // Экспортируем асинхронную функцию login
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    // Извлекаем username и password из тела запроса
+    const { username, password } = req.body;
+
+    // Находим пользователя в базе данных по username
+    const user = await User.findOne({ username });
+
+    // Если пользователь не найден, возвращаем сообщение об ошибке
+    if (!user) {
+      return res.json({
+        message: 'Такого пользователя не существует',
+      });
+    }
+
+    // Сравниваем введённый пароль с захешированным паролем в базе данных
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    // Если пароль неверный, возвращаем сообщение об ошибке
+    if (!isPasswordCorrect) {
+      return res.json({
+        message: 'Неверный пароль',
+      });
+    }
+
+    // Создаём JWT-токен с id пользователя, используя секретный ключ и задавая срок действия токена
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '30d' } // Токен будет действовать 30 дней
+    );
+
+    // Возвращаем клиенту токен, данные пользователя и сообщение об успешном входе
+    res.json({
+      token,
+      user,
+      message: 'Вы вошли в систему',
+    });
+  } catch (error) {
+    // Обрабатываем возможные ошибки
+    console.error(error);
+    res.json({
+      message: 'При авторизации произошла ошибка',
+    });
+  }
+};
 
 // Функция получения информации о текущем пользователе
 export const getMe = async (req, res) => {
